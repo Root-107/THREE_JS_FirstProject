65a44f95832cc7b7a3595f14a8a7504c272ba7bf
Root <sam.pugh@dbpixelhouse.com>
2020-03-24 21:22:23 +0000

    raw three

--- 3js-playtime/src/App.js
+++ 3js-playtime/src/App.js
@@ -1,36 +1,51 @@
 import React from 'react';
 import './App.css';
 
-import { Canvas, extend } from "react-three-fiber";
+import { Canvas, extend, render } from "react-three-fiber";
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import Stand from "./components/Modles";
-import Ambient from "./components/Lights/ambinet.js";
-import SpotLight from "./components/Lights/spot.js"
-import Environemnt from "./components/Environemnt";
-import PointLight from './components/Lights/point.js';
-
-/**
- * inside the canvas to add fog
- * <fog attach="fog" args={["grey", 25, 30]} />
- */
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 
 extend({OrbitControls});
 
+var object;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const rednerer = new THREE.WebGLRenderer({ antialias: true });
+
+rednerer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(rednerer.domElement);
+
+
+var geo = new THREE.SphereGeometry(2,12,12);
+var material = new THREE.MeshBasicMaterial({color: 0x000000});
+var spher = new THREE.Mesh(geo, material);
+scene.add(spher);
+
+camera.position.z = 5;
+
+// var loader = new OBJLoader();
+// loader.load( '../src/static/Imports/3D/StandModel.obj', function ( obj ) {
+
+//     object = obj;
+//     object.scale.multiplyScalar( 0.8 );
+//     object.position.y = - 30;
+//     scene.add( object );
+
+// } );
+
+function animate()
+{
+    requestAnimationFrame(animate);
+    rednerer.render(scene, camera);
+}
+
+animate();
+
 function App() {
     return (
-        <Canvas 
-            camera={{position:[0,0,8]}} 
-            onCreated={({ gl }) => {
-                gl.shadowMap.enabled = true
-                gl.shadowMap.type = THREE.PCFSoftShadowMap;
-            }}
-        >
-            <Stand/>
-            <Ambient/>
-            <SpotLight pos={[-3,1,1]} shadows={true} fallOff={0.5} color={0x00aeff}/>
-            <SpotLight pos={[3,2,-3]} shadows={true} fallOff={0.5} color={0xff9c00}/>
-           <Environemnt/>
+        <Canvas>
         </Canvas>
     );
 }
--- 3js-playtime/src/components/Lights/ambinet.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default ()=>{
-    return (
-        <ambientLight intensity={0.5} color={0xffffff}/>
-    )
-};
